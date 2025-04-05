e0fc7f91336a7164f51d7516658627713d6f948c
viktor_sergeev <nupidlo@gmail.com>
2025-04-05 20:26:07 +0300

    Add http statuses 200, 400

--- backend/src/main/java/com/example/backend/controllers/ServiceRequestController.java
+++ backend/src/main/java/com/example/backend/controllers/ServiceRequestController.java
@@ -7,6 +7,8 @@ import com.example.backend.models.VacancyService;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceRequestController {
   @PostMapping("/service-request")
-  public ServiceResponse requestService(@RequestBody ServiceRequest request) {
+  public ResponseEntity<ServiceResponse> requestService(@RequestBody ServiceRequest request) {
+    // валидация данных формы
+    // TODO: нужны запросы в хранилище для валидации профессий и регионов
+    if (request.profession() == null || request.profession().trim().isEmpty()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+    if (request.amount() < 1) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    if (request.area() == null || request.area().trim().isEmpty()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+
     Map<Integer, VacancyService> vacancyServices = new HashMap<>();
     Map<Integer, ResumesAccessService> resumesAccessServices = new HashMap<>();
 
@@ -35,7 +47,7 @@ public class ServiceRequestController {
         4500.0
         ));
 
-    return new ServiceResponse(vacancyServices, resumesAccessServices);
+    return ResponseEntity.ok(new ServiceResponse(vacancyServices, resumesAccessServices));
     // return new ServiceResponse(vacancyServices, Collections.emptyMap());
   }
 }
