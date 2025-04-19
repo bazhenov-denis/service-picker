fa1f8aaa2e7fcc224008e0aaed06f2d6a3d43278
bazhenov-denis <denis.bazhenov2005@yandex.ru>
2025-04-19 22:23:41 +0300

    add parent_id and fixed controllers

--- backend/src/main/java/com/example/backend/DTO/AreaDTO.java
+++ backend/src/main/java/com/example/backend/DTO/AreaDTO.java
@@ -1,16 +1,30 @@
 package com.example.backend.DTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.ALWAYS)
+@JsonPropertyOrder({ "id", "parent_id", "name", "areas" })
 public class AreaDTO {
+
     private String id;
+
+    @JsonProperty("parent_id")
+    private String parentId;
     private String name;
-    private List<AreaDTO> areas; // вложенные области
+    @JsonProperty("areas")
+    private List<AreaDTO> areas; 
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
+
+    public String getParentId() { return parentId; }
+    public void   setParentId(String parentId) { this.parentId = parentId; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
