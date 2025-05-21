f7270ebe684e4709b7fbce5bca0e6625ef4863d3
Mikhail Kruglikov <mkruglicov@gmail.com>
2025-05-21 18:39:11 +0300

    fill tables of test data (4rd)

--- backend/src/main/resources/db/migration/V6__fill_tables_test_data.sql
+++ backend/src/main/resources/db/migration/V6__fill_tables_test_data.sql
@@ -1,21 +1,3 @@
-INSERT INTO offers (
-    product_id, tariff, code,
-    child_code_1, child_count_1,
-    period, region_id, profrole_group_id,
-    price_all, currency
-) VALUES
-(907283, 'DI', 'CIV', '1280', 12800, 30, 1620, 0, 15693600, 'RUR'),
-(907287, 'DI', 'CIV', '3200', 32000, 92, 1624, 0, 38624600, 'RUR'),
-(907304, 'DI', 'CIV', '700', 7000, 30, 1530, 0, 8512000, 'RUR'),
-(907308, 'DI', 'CIV', '700', 7000, 30, 1620, 0, 7256500, 'RUR');
-
-
-INSERT INTO region_area_mapping (id, price_region_id, area_id) VALUES
-(1, 1620, 77),
-(2, 1624, 78),
-(3, 1530, 66); 
-
-
 INSERT INTO profrole (id, name) VALUES
 (4, 'Автомойщик'),
 (8, 'Администратор');
