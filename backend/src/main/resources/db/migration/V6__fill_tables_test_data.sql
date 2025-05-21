8035dfbba59f5d0c744aaed0d9e25886cd32c151
Mikhail Kruglikov <mkruglicov@gmail.com>
2025-05-21 17:13:27 +0300

    fill tables of test data

--- backend/src/main/resources/db/migration/V6__fill_tables_test_data.sql
+++ backend/src/main/resources/db/migration/V6__fill_tables_test_data.sql
@@ -1,15 +1,13 @@
 INSERT INTO offers (
-    id, product_id, tariff, code,
+    product_id, tariff, code,
     child_code_1, child_count_1,
-    child_code_2, child_count_2,
-    child_code_3, child_count_3,
-    child_code_4, child_count_4,
     period, region_id, profrole_group_id,
     price_all, currency
 ) VALUES
-(1, 101, 'Standard', 'OFFER1', 'CH1', 2, 'CH2', 1, 'CH3', 1, 'CH4', 2, 12, 1, 1, 4999.99, 'RUB'),
-(2, 102, 'Premium',  'OFFER2', 'CH3', 3, 'CH4', 2, 'CH5', 1, 'CH6', 1, 6,  2, 2, 8999.00, 'RUB'),
-(3, 103, 'Basic',    'OFFER3', 'CH7', 1, 'CH8', 2, 'CH9', 1, 'CH10', 1, 1,  1, 1, 1999.99, 'RUB');
+(907283, 'DI', 'CIV', '1280', 12800, 30, 1620, 0, 15693600, 'RUR'),
+(907287, 'DI', 'CIV', '3200', 32000, 92, 1624, 0, 38624600, 'RUR'),
+(907304, 'DI', 'CIV', '700', 7000, 30, 1530, 0, 8512000, 'RUR'),
+(907308, 'DI', 'CIV', '700', 7000, 30, 1620, 0, 7256500, 'RUR');
 
 
 INSERT INTO region_area_mapping (id, price_region_id, area_id) VALUES
