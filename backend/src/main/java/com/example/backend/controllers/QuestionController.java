70a4a995f3c2215d06f7f9a3654b14a5b27dcd50
bazhenov-denis <denis.bazhenov2005@yandex.ru>
2025-05-24 20:54:10 +0300

    added openApi description

--- backend/src/main/java/com/example/backend/controllers/QuestionController.java
+++ backend/src/main/java/com/example/backend/controllers/QuestionController.java
@@ -2,10 +2,17 @@ package com.example.backend.controllers;
 
 import com.example.backend.DTO.QuestionDTO;
 import com.example.backend.services.QuestionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+@Tag(
+    name = "Questions",
+    description = "Операции для работы с вопросами"
+)
 
 @RestController
 @RequestMapping("/questions")
@@ -13,6 +20,10 @@ public class QuestionController {
   private final QuestionService questionService;
   public QuestionController(QuestionService questionService) { this.questionService = questionService; }
 
+  @Operation(
+      summary = "Получить список всех вопросов",
+      description = "Возвращает массив DTO с полным набором вопросов"
+  )
   @GetMapping
   public List<QuestionDTO> list() {
     return questionService.getAllQuestions();
