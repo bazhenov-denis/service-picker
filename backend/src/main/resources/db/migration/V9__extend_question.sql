40e648956830c6666f762f40f4d715e507270dc4
bazhenov-denis <denis.bazhenov2005@yandex.ru>
2025-05-26 21:15:47 +0300

    refactoring

--- backend/src/main/resources/db/migration/V9__extend_question.sql
+++ backend/src/main/resources/db/migration/V9__extend_question.sql
@@ -3,27 +3,31 @@ ALTER TABLE questions
     ADD COLUMN position    INT;
 
 ALTER TABLE options
-    ADD COLUMN position INT;
+    ADD COLUMN position    INT;
 
 TRUNCATE TABLE options CASCADE;
 TRUNCATE TABLE questions CASCADE;
 
 -- 2) Вставляем заново, сразу со всеми новыми колонками
-INSERT INTO questions (id, type, question_text, reference_type, is_required,
-                       short_title, position)
-VALUES (1, 'reference', 'Выберите регион:', TRUE,
-        'Регион:', 'regions', 1),
-       (2, 'reference', 'Выберите профессию:', 'professions', TRUE,
-        'Профессия', 2),
-       (3, 'input', 'Сколько человек необходимо?', NULL, TRUE,
-        'Количество', 3),
-       (4, 'single-choice', 'Как срочно нужен человек?', NULL, FALSE,
-        'Срочность', 4)
+INSERT INTO questions (
+    id, type, question_text, reference_type, is_required,
+    short_title, position
+) VALUES
+      (1, 'reference', 'Выберите регион:',    'regions',     TRUE,
+       'Регион:', 1),
+      (2, 'reference', 'Выберите профессию:', 'professions', TRUE,
+       'Профессия',  2),
+      (3, 'input',     'Сколько человек необходимо?', NULL,   TRUE,
+       'Количество',  3),
+      (4, 'single-choice','Как срочно нужен человек?', NULL, FALSE,
+       'Срочность', 4)
 ;
 
-INSERT INTO options (id, question_id, text, position)
-VALUES (1, 4, 'как можно скорее', 1),
-       (2, 4, 'ближайшая неделя', 2),
-       (3, 4, 'ближайший месяц', 3),
-       (4, 4, 'не срочно', 4)
+INSERT INTO options (
+    id, question_id, text, position
+) VALUES
+      (1, 4, 'как можно скорее', 1),
+      (2, 4, 'ближайшая неделя', 2),
+      (3, 4, 'ближайший месяц', 3),
+      (4, 4, 'не срочно',         4)
 ;
