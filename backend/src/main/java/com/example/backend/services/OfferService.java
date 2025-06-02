8727da9bbb53192013eb1f1506371ff8c7ed177b
bazhenov-denis <denis.bazhenov2005@yandex.ru>
2025-06-02 12:54:38 +0300

    refactoring

--- backend/src/main/java/com/example/backend/services/OfferService.java
+++ backend/src/main/java/com/example/backend/services/OfferService.java
@@ -14,9 +14,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -40,8 +37,6 @@ public class OfferService {
     this.priceProfroleService = priceProfroleService;
     this.priceRegionService = priceRegionService;
   }
-
-  private static final Logger log = LoggerFactory.getLogger(OfferService.class);
 
   public OfferListDto pick(ClaimDto claim) {
     log.info("Начало pick(), claim = {}", claim);
