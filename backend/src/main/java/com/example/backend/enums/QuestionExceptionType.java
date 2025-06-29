5b724f5f33b74cbc4b7d14b390736dfc7b6afa9d
viktor_sergeev <nupidlo5@gmail.com>
2025-06-29 21:06:51 +0500

    Add updating questions with multiple checks

--- backend/src/main/java/com/example/backend/enums/QuestionExceptionType.java
+++ backend/src/main/java/com/example/backend/enums/QuestionExceptionType.java
@@ -4,7 +4,12 @@ public enum QuestionExceptionType {
   INVALID_TYPE("Invalid question type"),
   INVALID_REFERENCE_TYPE("Invalid question reference type"),
   NOT_ENOUGH_OPTIONS("Not enough options provided"),
-  WRONG_NUMBER_OF_SCORES("Wrong quantity of scores provided");
+  WRONG_NUMBER_OF_SCORES("Wrong quantity of scores provided"),
+  WRONG_ORDER("No order can be made with new positions"),
+  DUPLICATED_ID("Several identical ids"),
+  WRONG_ID("Id doesn't exist"),
+  WRONG_POSITION("Position doesn't exist"),
+  NULL_FIELD("Null fields are not allowed");
 
   private final String msg;
 
